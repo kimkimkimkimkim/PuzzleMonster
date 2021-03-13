5cd50bb7006fea2b79bf68f9ae0844d955f6854e
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-03-13 19:41:23 +0900

    ログイン機能実装

--- Assets/Scripts/Manager/MainSceneManager.cs
+++ Assets/Scripts/Manager/MainSceneManager.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using GameBase;
 using UniRx;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class MainSceneManager : SingletonMonoBehaviour<MainSceneManager>
 {
     private void Start()
     {
-        GameWindowFactory.Create(new GameWindowRequest()).Subscribe();
+        //GameWindowFactory.Create(new GameWindowRequest()).Subscribe();
+        PlayFabClientAPI.LoginWithCustomID(
+            new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true }
+        , result => Debug.Log("おめでとうございます！ログイン成功です！")
+        , error => Debug.Log("ログイン失敗...(´；ω；｀)"));
     }
 }
