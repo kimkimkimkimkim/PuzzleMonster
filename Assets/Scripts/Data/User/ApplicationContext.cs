b79ca12bc8047f6c5fef3f0cc2f730a278ab566a
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-03-27 21:36:01 +0900

    ApplicationContextの作成

---
+++ Assets/Scripts/Data/User/ApplicationContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Enum.Item;
+using PlayFab.ClientModels;
+using UniRx;
+
+public static class ApplicationContext
+{
+    public static UserDataInfo userData { get; private set; } = new UserDataInfo();
+
+    /// <summary>
+    /// アプリ起動時処理
+    /// </summary>
+    public static IObservable<Unit> EstablishSession()
+    {
+        return ApiConnection.LoginWithCustomID()
+            .SelectMany(_ => ApiConnection.GetPlayerProfile().Do(res =>
+            {
+                // ユーザープロフィールの同期
+                userData.playerProfile = res.PlayerProfile;
+            }))
+            .SelectMany(_ => ApiConnection.GetUserInventory().Do(res =>
+            {
+                // ユーザーインベントリ情報の更新
+                UpdateUserInventory(res);
+            }))
+            .AsUnitObservable();
+    }
+
+    /// <summary>
+    /// ユーザーインベントリ情報の更新
+    /// </summary>
+    public static void UpdateUserInventory(GetUserInventoryResult result)
+    {
+        // 仮想通貨情報の更新
+        foreach (var virtualCurrency in result.VirtualCurrency)
+        {
+            if (virtualCurrency.Key == VirtualCurrencyType.OB.ToString()) userData.userVirtualCurrency.orb = virtualCurrency.Value;
+            if (virtualCurrency.Key == VirtualCurrencyType.CN.ToString()) userData.userVirtualCurrency.coin = virtualCurrency.Value;
+        }
+    }
+}
---
+++ Assets/Scripts/Data/User/UserDataInfo.cs
@@ -0,0 +1,16 @@
+﻿using PlayFab.ClientModels;
+/// <summary>
+/// ユーザーデータ情報
+/// </summary>
+public class UserDataInfo
+{
+    /// <summary>
+    /// ユーザープロフィール情報
+    /// </summary>
+    public PlayerProfileModel playerProfile { get; set; } = new PlayerProfileModel();
+
+    /// <summary>
+    /// ユーザー仮想通貨情報
+    /// </summary>
+    public UserVirtualCurrencyInfo userVirtualCurrency { get; set; } = new UserVirtualCurrencyInfo();
+}
