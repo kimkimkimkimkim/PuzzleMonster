c8f5e1b83776811da763f02e32a6c51707cc91ce
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-06-16 23:50:50 +0900

    FadeManagerの作成

---
+++ Assets/Scripts/Manager/FadeManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GameBase;
+using UniRx;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeManager : SingletonMonoBehaviour<FadeManager>
+{
+    [SerializeField] FadeImage _fade;
+
+    private const float ANIMATION_TIME = 1.0f;
+
+    private void Start()
+    {
+        _fade.Range = 0.0f;
+    }
+
+    public IObservable<Unit> PlayFadeAnimationObservable(float toValue)
+    {
+        return PlayFadeAnimationObservable(toValue, ANIMATION_TIME);
+    }
+
+    public IObservable<Unit> PlayFadeAnimationObservable(float toValue, float animationTime)
+    {
+        return DOVirtual.Float(_fade.Range, toValue, animationTime, value => _fade.Range = value)
+            .OnCompleteAsObservable()
+            .AsUnitObservable();
+    }
+}
+
