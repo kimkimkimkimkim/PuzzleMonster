b4c4f4d9eb661eed15aac2f6ede8b7e17fdcde68
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-06-17 22:20:01 +0900

    ドラッグ処理の実装

---
+++ Assets/Scripts/UI/Parts/BattlePieceItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BattlePieceItem : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    public void OnPointerDown(PointerEventData data)
+    {
+        Debug.Log("touch");
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        var targetPos = Camera.main.ScreenToWorldPoint(data.position);
+        targetPos.z = 0;
+        transform.position = targetPos;
+    }
+}
