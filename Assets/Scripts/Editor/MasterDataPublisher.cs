830ad3b54c548019bd0396fbfbf2c6ac0b555146
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-07-20 23:46:21 +0900

    最初のシートのマスタデータをJsonに変換できるように

--- Assets/Scripts/Editor/MasterDataPublisher.cs
+++ Assets/Scripts/Editor/MasterDataPublisher.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEditor;
-using System;
 using UnityEngine;
 using System.IO;
-using System.Collections.Generic;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
+using Newtonsoft.Json;
 
 /// <summary>
 /// 指定パスに存在するExcelファイルを参考にマスターデータ用のJsonを作成しPlayFabにアップロードする
 /// </summary>
-public class MasterDataPublisher : EditorWindow {
+public class MasterDataPublisher : EditorWindow
+{
     public static int NAME_ROW_INDEX = 1; // マスタ名が記述されたセルの行インデックス
     public static int NAME_COLUMN_INDEX = 1; // マスタ名が記述されたセルの列インデックス
     public static int HIERARCHY_1_ROW_INDEX = 3; // 階層1が記述されたセルの行インデックス（変数名）
@@ -19,16 +19,19 @@ public class MasterDataPublisher : EditorWindow {
     public static int START_DATA_ROW_INDEX = 9; // データ記述が開始される行インデックス
     public static int START_DATA_COLUMN_INDEX = 1; // データ記述が開始される行インデックス
 
-    private string filePath = $"Assets/Excel/MasterData.xlsx";
+    private string filePath = $"Assets/Excel/MasterData.xlsm";
 
     [MenuItem("Tools/MasterDataPublisher")]
-    static void Init() {
+    static void Init()
+    {
         var window = GetWindow<MasterDataPublisher>(typeof(MasterDataPublisher));
         window.Show();
     }
 
-    public void OnGUI() {
-        if (GUILayout.Button("チェック", new GUILayoutOption[] { })) {
+    public void OnGUI()
+    {
+        if (GUILayout.Button("チェック", new GUILayoutOption[] { }))
+        {
             var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             var book = new XSSFWorkbook(fs);
 
@@ -40,44 +43,52 @@ public class MasterDataPublisher : EditorWindow {
 
             var rowIndex = START_DATA_ROW_INDEX;
             var jsonStr = "[";
-            while (GetValueStr(sheet, rowIndex, START_DATA_COLUMN_INDEX) != "") {
+            while (GetValueStr(sheet, rowIndex, START_DATA_COLUMN_INDEX) != "")
+            {
                 // 各データに対する処理開始
                 var columnIndex = 1;
-                jsonStr += "\n  {";
-                while (GetTypeStr(sheet, columnIndex) != "") {
+                jsonStr += "{";
+                while (GetTypeStr(sheet, columnIndex) != "")
+                {
                     // 各値に対する処理開始
                     var type = GetTypeStr(sheet, columnIndex);
 
                     // 階層1が空ならスキップ
                     var key1 = GetHierarchyStr(sheet, HIERARCHY_1_ROW_INDEX, columnIndex);
-                    if (key1 == "") {
+                    if (key1 == "")
+                    {
                         columnIndex++;
                         continue;
                     }
 
                     // 階層1が空じゃないならKeyValue文字列を取得
                     var keyValueStr = GetKeyValueStr(sheet, rowIndex, columnIndex);
-                    if (keyValueStr != "") {
-                        jsonStr += $"\n    {keyValueStr}";
+                    if (keyValueStr != "")
+                    {
+                        jsonStr += $"{keyValueStr}";
                         jsonStr += ",";
                     }
                     columnIndex++;
                 }
                 jsonStr = jsonStr.Remove(jsonStr.Length - 1);
-                jsonStr += "\n  },";
+                jsonStr += "},";
                 rowIndex++;
             }
             jsonStr = jsonStr.Remove(jsonStr.Length - 1);
-            jsonStr += "\n]";
+            jsonStr += "]";
 
+            var parsedJson = JsonConvert.DeserializeObject(jsonStr);
+            var formattedJsonStr = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
             Debug.Log(jsonStr);
+            Debug.Log(formattedJsonStr);
         }
     }
 
     /// <summary>
     /// セル情報を取得
     /// </summary>
-    private ICell GetCell(ISheet sheet, int rowIndex, int columnIndex) {
+    private ICell GetCell(ISheet sheet, int rowIndex, int columnIndex)
+    {
         return sheet.GetRow(rowIndex).GetCell(columnIndex);
     }
 
@@ -86,12 +97,13 @@ public class MasterDataPublisher : EditorWindow {
     /// 元々文字列ならダブルクォーテーションをつける
     /// 空なら空文字を返す
     /// </summary>
-    private string GetValueStr(ISheet sheet, int rowIndex, int columnIndex) {
+    private string GetValueStr(ISheet sheet, int rowIndex, int columnIndex)
+    {
         var type = sheet?.GetRow(TYPE_ROW_INDEX)?.GetCell(columnIndex)?.StringCellValue;
         if (type == null) return "";
 
         var cell = sheet?.GetRow(rowIndex)?.GetCell(columnIndex);
-        if (cell.CellType == CellType.Blank) return "";
+        if (cell == null || cell.CellType == CellType.Blank) return "";
 
         return GetValueStr(cell, type);
     }
@@ -101,22 +113,27 @@ public class MasterDataPublisher : EditorWindow {
     /// 元々文字列ならダブルクォーテーションをつける
     /// 空なら空文字を返す
     /// </summary>
-    private string GetValueStr(ICell cell, string type) {
+    private string GetValueStr(ICell cell, string type)
+    {
         if (cell == null) return "";
 
-        try {
-            switch (type) {
+        try
+        {
+            switch (type)
+            {
                 case "int":
                 case "float":
                     return cell.NumericCellValue.ToString();
                 case "boolean":
-                    return cell.BooleanCellValue.ToString();
+                    return cell.BooleanCellValue.ToString().ToLower();
                 case "string":
                     return $"\"{cell.StringCellValue}\"";
                 default:
                     return cell.StringCellValue;
             }
-        } catch {
+        }
+        catch
+        {
             return "";
         }
     }
@@ -124,7 +141,8 @@ public class MasterDataPublisher : EditorWindow {
     /// <summary>
     /// 指定した列の型名を返します
     /// </summary>
-    private string GetTypeStr(ISheet sheet, int columnIndex) {
+    private string GetTypeStr(ISheet sheet, int columnIndex)
+    {
         var type = sheet?.GetRow(TYPE_ROW_INDEX)?.GetCell(columnIndex)?.StringCellValue ?? "";
         return type;
     }
@@ -132,12 +150,14 @@ public class MasterDataPublisher : EditorWindow {
     /// <summary>
     /// 階層行の値を文字列で返します
     /// </summary>
-    private string GetHierarchyStr(ISheet sheet, int rowIndex, int columnIndex) {
+    private string GetHierarchyStr(ISheet sheet, int rowIndex, int columnIndex)
+    {
         if (rowIndex != HIERARCHY_1_ROW_INDEX && rowIndex != HIERARCHY_2_ROW_INDEX && rowIndex != HIERARCHY_3_ROW_INDEX) return "";
 
         var cell = sheet?.GetRow(rowIndex)?.GetCell(columnIndex);
 
-        switch (cell.CellType) {
+        switch (cell.CellType)
+        {
             case CellType.String:
                 return $"\"{cell.StringCellValue}\"";
             case CellType.Numeric:
@@ -152,21 +172,24 @@ public class MasterDataPublisher : EditorWindow {
     /// 指定したセルのKeyValue文字列を返します
     /// 指定する列の階層1の行に値が無い場合は空文字を返します
     /// </summary>
-    private string GetKeyValueStr(ISheet sheet, int rowIndex, int columnIndex) {
+    private string GetKeyValueStr(ISheet sheet, int rowIndex, int columnIndex)
+    {
         // 指定したセルの階層1の行に値が無い場合は空文字を返す
         var key1 = GetHierarchyStr(sheet, HIERARCHY_1_ROW_INDEX, columnIndex);
         if (key1 == "") return "";
 
         var key2 = GetHierarchyStr(sheet, HIERARCHY_2_ROW_INDEX, columnIndex);
         var cell2 = GetCell(sheet, HIERARCHY_2_ROW_INDEX, columnIndex);
-        switch (cell2.CellType) {
+        switch (cell2.CellType)
+        {
             case CellType.Blank:
                 // 空ならそのままKeyValue文字列を作成
                 var value = GetValueStr(sheet, rowIndex, columnIndex);
                 return value != "" ? $"{key1}:{value}" : "";
             case CellType.Numeric:
                 // 数値ならリストとして処理
-                return "list";
+                var listValue = GetListValueStr(sheet, rowIndex, columnIndex);
+                return listValue != "" ? $"{key1}:{listValue}" : "";
             case CellType.String:
                 // 文字列ならオブジェクトとして処理
                 var objectValue = GetObjectValueStr(sheet, rowIndex, columnIndex);
@@ -180,7 +203,8 @@ public class MasterDataPublisher : EditorWindow {
     /// 値の中のオブジェクトをValue文字列として返します
     /// オブジェクトの最初の要素のセルインデックスを指定する
     /// </summary>
-    private string GetObjectValueStr(ISheet sheet, int rowIndex, int columnIndex) {
+    private string GetObjectValueStr(ISheet sheet, int rowIndex, int columnIndex)
+    {
         var cell2 = GetCell(sheet, HIERARCHY_2_ROW_INDEX, columnIndex);
         var cell3 = GetCell(sheet, HIERARCHY_3_ROW_INDEX, columnIndex);
 
@@ -192,10 +216,12 @@ public class MasterDataPublisher : EditorWindow {
 
         var jsonStr = "{";
         var lastColumnIndex = sheet.GetRow(TYPE_ROW_INDEX).LastCellNum - 1;
-        do {
+        do
+        {
             var key = GetHierarchyStr(sheet, hierarchyRowIndex, columnIndex);
             var value = GetValueStr(sheet, rowIndex, columnIndex);
-            if (value != "") {
+            if (value != "")
+            {
                 jsonStr += $"{key}:{value},";
             }
 
@@ -212,21 +238,57 @@ public class MasterDataPublisher : EditorWindow {
     /// リストの最初の要素のセルインデックスを指定する
     /// 現状リストの要素番号は階層2にしか来ない
     /// </summary>
-    private string GetListValueStr(ISheet sheet, int rowIndex, int columnIndex) {
+    private string GetListValueStr(ISheet sheet, int rowIndex, int columnIndex)
+    {
         // 階層2の値が数値でなければ空文字を返す
         if (GetCell(sheet, HIERARCHY_2_ROW_INDEX, columnIndex).CellType != CellType.Numeric) return "";
 
+        var isObject = GetHierarchyStr(sheet, HIERARCHY_3_ROW_INDEX, columnIndex) != "";
+
         var jsonStr = "[";
         var lastColumnIndex = sheet.GetRow(TYPE_ROW_INDEX).LastCellNum - 1;
-        do {
-            do {
+        do
+        {
+            var elementJsonStr = "";
+            do
+            {
+                var key = GetHierarchyStr(sheet, HIERARCHY_3_ROW_INDEX, columnIndex);
+                var value = GetValueStr(sheet, rowIndex, columnIndex);
+
+                if (key == "")
+                {
+                    // キーが空文字ならオブジェクトじゃない普通の値
+                    if (value != "")
+                    {
+                        elementJsonStr += $"{value},";
+                    }
+                }
+                else
+                {
+                    // キーが空文字じゃないならオブジェクト
+                    if (value != "")
+                    {
+                        elementJsonStr += $"{key}:{value},";
+                    }
+                }
 
                 columnIndex++;
-            } while (GetCell(sheet, HIERARCHY_2_ROW_INDEX, columnIndex).CellType == CellType.Blank && columnIndex <= lastColumnIndex);
-        } while (GetCell(sheet, HIERARCHY_1_ROW_INDEX, columnIndex).CellType == CellType.Blank && columnIndex <= lastColumnIndex);
+            } while (GetCell(sheet, HIERARCHY_1_ROW_INDEX, columnIndex)?.CellType == CellType.Blank && GetCell(sheet, HIERARCHY_2_ROW_INDEX, columnIndex)?.CellType == CellType.Blank && columnIndex <= lastColumnIndex);
+
+            if (elementJsonStr != "")
+            {
+                elementJsonStr = elementJsonStr.Remove(elementJsonStr.Length - 1);
+                if (isObject)
+                {
+                    elementJsonStr = elementJsonStr.Insert(0, "{");
+                    elementJsonStr += "}";
+                }
+                jsonStr += $"{elementJsonStr},";
+            }
+        } while (GetCell(sheet, HIERARCHY_1_ROW_INDEX, columnIndex)?.CellType == CellType.Blank && columnIndex <= lastColumnIndex);
         jsonStr = jsonStr.Remove(jsonStr.Length - 1);
         jsonStr += "]";
 
-        return jsonStr;
+        return jsonStr != "]" ? jsonStr : "";
     }
 }
