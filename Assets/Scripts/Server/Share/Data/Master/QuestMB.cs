62652640a7f02894789d16a6b6491f6b675ac0db
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-07-31 21:36:39 +0900

    QuestMBの修正

--- Assets/Scripts/Server/Share/Data/Master/QuestMB.cs
+++ Assets/Scripts/Server/Share/Data/Master/QuestMB.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 /// <summary>
 /// クエストマスタ
@@ -25,4 +26,19 @@ public class QuestMB : MasterBookBase
     /// ドロップアイテムバンドルID
     /// </summary>
     public long dropBundleId { get; set; }
+
+    /// <summary>
+    /// phase1のクエストモンスターIDリスト
+    /// </summary>
+    public List<long> phase1QuestMonsterIdList { get; set; }
+
+    /// <summary>
+    /// phase2のクエストモンスターIDリスト
+    /// </summary>
+    public List<long> phase2QuestMonsterIdList { get; set; }
+
+    /// <summary>
+    /// phase3のクエストモンスターIDリスト
+    /// </summary>
+    public List<long> phase3QuestMonsterIdList { get; set; }
 }
