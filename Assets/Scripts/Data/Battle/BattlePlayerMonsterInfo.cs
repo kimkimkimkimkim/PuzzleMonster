d498c2ce0840ee18aa492922fd92f590196ab700
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-08-05 15:03:37 +0900

    Update BattlePlayerMonsterInfo.cs

--- Assets/Scripts/Data/Battle/BattlePlayerMonsterInfo.cs
+++ Assets/Scripts/Data/Battle/BattlePlayerMonsterInfo.cs
@@ -6,6 +6,11 @@ public class BattlePlayerMonsterInfo
     public long monsterId;
     
     /// <summary>
+    /// 体力
+    /// </summary>
+    public int hp;
+    
+    /// <summary>
     /// 攻撃力
     /// </summary>
     public int attack;
