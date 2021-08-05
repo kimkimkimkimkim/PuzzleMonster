8226a91f5db4bfa80b5c19b025a89288d4b3d9c9
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-08-05 14:50:47 +0900

    Create BattlePlayerMonsterInfo.cs

---
+++ Assets/Scripts/Data/Battle/BattlePlayerMonsterInfo.cs
@@ -0,0 +1,12 @@
+public class BattlePlayerMonsterInfo
+{
+    /// <summary>
+    /// モンスターID
+    /// </summary>
+    public long monsterId;
+    
+    /// <summary>
+    /// 攻撃力
+    /// </summary>
+    public int attack;
+}
