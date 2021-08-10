e3de64a06ba09f4599901f6662e55af788641bcd
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-08-10 20:05:13 +0900

    usingの追加

--- Assets/Scripts/Vfx/QuestTitleFx.cs
+++ Assets/Scripts/Vfx/QuestTitleFx.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using GameBase;
 using DG.Tweening;
+using UnityEngine.AddressableAssets;
 
 public class QuestTitleFx : MonoBehaviour
 {
--- Assets/Scripts/Vfx/WaveTitleFx.cs
+++ Assets/Scripts/Vfx/WaveTitleFx.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using GameBase;
 using DG.Tweening;
-
+using UnityEngine.AddressableAssets;
 
 public class WaveTitleFx : MonoBehaviour
 {
