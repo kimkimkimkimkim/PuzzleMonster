a9f43458615cbc08f9252dc0bcefb6ae3dd691a6
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-08-10 11:02:17 +0900

    Update QuestTitleFx.cs

--- Assets/Scripts/Vfx/QuestTitleFx.cs
+++ Assets/Scripts/Vfx/QuestTitleFx.cs
@@ -9,10 +9,11 @@ public class QuestTitleFx : MonoBehaviour
 {
     [SerializeField] Text _text;
 
-    public IObservable<Unit> PlayFxObservable()
+    public IObservable<Unit> PlayFxObservable(string title)
     {
-        gameObject.SetActive(true);
         _text.SetAlpha(0);
+        _text.text = title;
+        gameObject.SetActive(true);
 
         return DOTween.Sequence()
             .Append(DOVirtual.Float(0.0f, 1.0f, 1.0f, value => _text.SetAlpha(value)))
