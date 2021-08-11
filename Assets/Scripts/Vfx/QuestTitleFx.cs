4751fa64c04a6a5cac716d1a7bb8e65b51d5ff0b
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-08-11 12:14:33 +0900

    Update QuestTitleFx.cs

--- Assets/Scripts/Vfx/QuestTitleFx.cs
+++ Assets/Scripts/Vfx/QuestTitleFx.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
-using GameBase;
-using DG.Tweening;
-using UnityEngine.AddressableAssets;
 
 public class QuestTitleFx : MonoBehaviour
 {
-    public Text _text;
+    public Text text;
 }
