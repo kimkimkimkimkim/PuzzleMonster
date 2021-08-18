73ccf5657a59443e380ff0f2aa381fd959d43592
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-08-18 18:35:24 +0900

    WinBattleFxの作成

---
+++ Assets/Scripts/Vfx/WinBattleFx.cs
@@ -0,0 +1,10 @@
+  
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class WinBattleFx : MonoBehaviour
+{
+    public List<Text> textList;
+    public Transform textInitialPositionTransform;
+}
