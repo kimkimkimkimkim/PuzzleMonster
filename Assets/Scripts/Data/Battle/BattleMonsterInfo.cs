aa4938981a8fdfdce59d0369a7315cb84415541d
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-10-20 14:45:42 +0900

    BattleMonsterInfoの修正

--- Assets/Scripts/Data/Battle/BattleMonsterInfo.cs
+++ Assets/Scripts/Data/Battle/BattleMonsterInfo.cs
@@ -1,12 +1,59 @@
-﻿public class BattleMonsterInfo
+using System.Collections.Generic;
+
+public class BattleMonsterInfo
 {
+    /// <summary>
+    /// レベル
+    /// </summary>
+    public int level;
+     
+    /// <summary>
+    /// 最大体力
+    /// </summary>
+    public int maxHp;
+     
     /// <summary>
     /// 現在の体力
     /// </summary>
     public int currentHp;
 
     /// <summary>
-    /// 基本攻撃力
+    /// 基準攻撃力
     /// </summary>
     public int baseAttack;
+     
+    /// <summary>
+    /// 現在の攻撃力
+    /// </summary>
+    public float currentAttack;
+     
+    /// <summary>
+    /// 基準スピード
+    /// </summary>
+    public int baseSpeed;
+     
+    /// <summary>
+    /// 現在のスピード
+    /// </summary>
+    public float currentSpeed;
+     
+    /// <summary>
+    /// 基準回復力
+    /// </summary>
+    public int baseHeal;
+     
+    /// <summary>
+    /// 現在の回復力
+    /// </summary>
+    public float currentHeal;
+     
+    /// <summary>
+    /// 現在のクールタイム
+    /// </summary>
+    public float currentCt;
+     
+    /// <summary>
+    /// 状態異常リスト
+    /// </summary>
+    public List<BattleConditionInfo> battleConditionList;
 }
