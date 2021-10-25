05ca0d7d1c3b5c06ca7b0b7a8da955f421173736
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-10-25 17:08:25 +0900

    BattleMonsterIndexの作成

---
+++ Assets/Scripts/Data/Battle/BattleMonsterIndex.cs
@@ -0,0 +1,11 @@
+public class BattleMonsterIndex {
+    /// <summary>
+    /// プレイヤーのモンスターか否か
+    /// </summary>
+    public bool isPlayer { get; set; }
+    
+    /// <summary>
+    /// インデックス
+    /// </summary>
+    public int index { get; set; }
+}
