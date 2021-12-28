d96bbc1ff3bd5cd3c73ac631633f7c1dc2619780
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-12-28 12:01:30 +0900

    バトルログ出力処理の修正

--- Assets/Scripts/Manager/BattleDataProcessor.cs
+++ Assets/Scripts/Manager/BattleDataProcessor.cs
@@ -9,27 +9,14 @@ public class BattleDataProcessor
     private int currentWaveCount;
     private int currentTurnCount;
     private QuestMB quest;
+    private List<BattleLogInfo> battleLogList = new List<BattleLogInfo>();
     private List<BattleMonsterInfo> playerBattleMonsterList = new List<BattleMonsterInfo>();
     private List<BattleMonsterInfo> enemyBattleMonsterList = new List<BattleMonsterInfo>();
     private BattleMonsterIndex doBattleMonsterIndex;
     private BattleMonsterIndex beDoneBattleMonsterIndex;
+    private BattleMonsterActType doBattleMonsterActType;
     private WinOrLose currentWinOrLose;
-    
-    public List<BattleLogInfo> GetBattleLogList(UserMonsterPartyInfo userMonsterParty, QuestMB quest)
-    {
-        Init(userMonsterParty, quest);
-        
-        var battleLogList = new List<BattleLogInfo>();
-
-        // 最初にWave1開始のログを追加する
-        battleLogList.Add(GetCurrentBattleLogInfo(BattleLogType.MoveWave)); 
-
-        while(currentWinOrLose == WinOrLose.Continue){
-            battleLogList.AddRange(CalculateBattleLogList());
-        }
-        
-        return battleLogList;
-    }
+    private BattlePhase currentBattlePhase;
 
     private void Init(UserMonsterPartyInfo userMonsterParty, QuestMB quest)
     {
@@ -45,124 +32,193 @@ public class BattleDataProcessor
         RefreshEnemyBattleMonsterList(currentWaveCount);
     }
 
-    /// <summary>
-    /// 現在のメンバ変数の状態からバトルログ情報を計算する
-    /// </summary>
-    private List<BattleLogInfo> CalculateBattleLogList(){
-        currentWinOrLose = GetWinOrLose();
-        if(currentWinOrLose != WinOrLose.Continue)
+    public List<BattleLogInfo> GetBattleLogList(UserMonsterPartyInfo userMonsterParty, QuestMB quest)
+    {
+        Init(userMonsterParty, quest);
+
+        // 最初にウェーブ移動とターン移動のログを入れておく
+        AddBattleLog(BattleLogType.MoveWave);
+        AddBattleLog(BattleLogType.MoveTurn);
+
+        // 勝敗が決まるまで続ける
+        while (currentWinOrLose == WinOrLose.Continue)
         {
-            // 勝敗が決まっていたら終了
-            return new List<BattleLogInfo>() { GetCurrentBattleLogInfo(BattleLogType.Result) };
+            ExecuteBattlePhase();
         }
 
-        // 勝敗が決まっていないかつ敵が全滅していたら次のウェーブへ
+        return battleLogList;
+    }
+
+    private void ExecuteBattlePhase()
+    {
+        switch (currentBattlePhase)
+        {
+            case BattlePhase.SetDoMonster:
+                ExecuteSetDoMonsterPhase();
+                break;
+            case BattlePhase.MoveNextWave:
+                ExecuteMoveNextWavePhase();
+                break;
+            case BattlePhase.MoveNextTurn:
+                ExecuteMoveNextTurnPhase();
+                break;
+            case BattlePhase.SetDoSkill:
+                ExecuteSetDoSkillPhase();
+                break;
+            case BattlePhase.SetBeDoneMonster:
+                ExecuteSetBeDoneMonsterPhase();
+                break;
+            case BattlePhase.ActivateSkill:
+                ExecuteActivateSkillPhase();
+                break;
+            case BattlePhase.ReflectSkill:
+                ExecuteReflectSkillPhase();
+                break;
+            case BattlePhase.JudgeBattleEnd:
+                ExecuteJudgeBattleEndPhase();
+                break;
+        }
+
+        currentBattlePhase = GetNextBattlePhase(currentBattlePhase);
+    }
+
+    /// <summary>
+    /// 次行動するモンスターのモンスターインデックスを設定する
+    /// nullの場合もある
+    /// </summary>
+    private void ExecuteSetDoMonsterPhase()
+    {
+        // 敵味方含めたバトルモンスターリストを取得
+        var battleMonsterList = new List<BattleMonsterInfo>(playerBattleMonsterList);
+        battleMonsterList.AddRange(enemyBattleMonsterList);
+
+        // まだ行動していないかつスピードが一番早いモンスターを取得
+        var battleMonster = battleMonsterList.Where(m => !m.isActed && m.currentHp > 0).OrderByDescending(m => m.currentSpeed).FirstOrDefault();
+        doBattleMonsterIndex = battleMonster?.index;
+    }
+
+    /// <summary>
+    /// 必要であれば次のウェーブに進む
+    /// </summary>
+    private void ExecuteMoveNextWavePhase()
+    {
+        // 敵が全滅していたら次のウェーブへ
         if (enemyBattleMonsterList.All(m => m.currentHp <= 0))
         {
-            MoveNextWave();
-            return new List<BattleLogInfo>() { GetCurrentBattleLogInfo(BattleLogType.MoveWave) };
+            currentWaveCount++;
+            RefreshEnemyBattleMonsterList(currentWaveCount);
+
+            // ログ追加
+            AddBattleLog(BattleLogType.MoveWave);
         }
-
-        // 勝敗が決まっていないかつ敵が全滅していないかつ次行動できるモンスターがいない場合は次のターンへ
-        var nextActIndex = GetNextActBattleMonsterIndexOrDefault();
-        if(nextActIndex == null)
-        {
-            MoveNextTurn();
-            return new List<BattleLogInfo>() { GetCurrentBattleLogInfo(BattleLogType.MoveTurn) };
-        }
-
-        // それ以外の場合は、どのモンスターかが行動する
-        var actionLogList = new List<BattleLogInfo>();
-
-        // 攻撃開始ログ
-        doBattleMonsterIndex = nextActIndex;
-        beDoneBattleMonsterIndex = GetAttackTargetBattleMonsterIndex(doBattleMonsterIndex); // TODO: 攻撃対象取得処理の実装
-        var startAttackLog = GetCurrentBattleLogInfo(BattleLogType.StartAttack);
-        actionLogList.Add(startAttackLog);
-
-        // 被ダメージログ
-        var takeDamageLog = GetCurrentBattleLogInfo(BattleLogType.TakeDamage);
-        actionLogList.Add(takeDamageLog);
-
-        // 死亡ログ
-        var takeDamageBattleMonster = GetBattleMonster(beDoneBattleMonsterIndex);
-        if(takeDamageBattleMonster.currentHp <= 0)
-        {
-            var dieLog = GetCurrentBattleLogInfo(BattleLogType.Die);
-            actionLogList.Add(dieLog);
-        }
-
-        return actionLogList;
     }
 
-    private BattleMonsterInfo GetBattleMonster(BattleMonsterIndex battleMonsterIndex)
+    /// <summary>
+    /// 必要であれば次のターンに進む
+    /// </summary>
+    private void ExecuteMoveNextTurnPhase()
     {
-        var battleMonsterList = battleMonsterIndex.isPlayer ? playerBattleMonsterList : enemyBattleMonsterList;
-        return battleMonsterList.First(m => m.index.index == battleMonsterIndex.index);
+        // 次行動するモンスターがいなければ次のターンへ
+        if (doBattleMonsterIndex == null)
+        {
+            currentTurnCount++;
+            playerBattleMonsterList.ForEach(m => m.isActed = false);
+            enemyBattleMonsterList.ForEach(m => m.isActed = false);
+            doBattleMonsterIndex = null;
+            beDoneBattleMonsterIndex = null;
+
+            // ログ追加
+            AddBattleLog(BattleLogType.MoveTurn);
+        }
     }
-    
-    private BattleMonsterIndex GetAttackTargetBattleMonsterIndex(BattleMonsterIndex doBattleMonsterIndex)
+
+    /// <summary>
+    /// 次行動するモンスターの使用するスキルを決める
+    /// </summary>
+    private void ExecuteSetDoSkillPhase()
+    {
+        // CTが最大まで溜まっていればアルティメットスキルそうでなければ通常スキルを設定
+        var doBattleMonster = GetBattleMonster(doBattleMonsterIndex);
+        doBattleMonsterActType = doBattleMonster.currentCt >= ConstManager.Battle.MAX_CT_VALUE ? BattleMonsterActType.UltimateSkill : BattleMonsterActType.NormalSkill;
+    }
+
+    /// <summary>
+    /// 使用するスキルの対象を設定する
+    /// </summary>
+    private void ExecuteSetBeDoneMonsterPhase()
     {
         var battleMonsterList = doBattleMonsterIndex.isPlayer ? enemyBattleMonsterList : playerBattleMonsterList;
         var targetBattleMonsterInfo = battleMonsterList.Where(m => m.currentHp > 0).Shuffle().FirstOrDefault();
 
-        return targetBattleMonsterInfo != null ? targetBattleMonsterInfo.index : null;
+        beDoneBattleMonsterIndex = targetBattleMonsterInfo != null ? targetBattleMonsterInfo.index : null;
     }
 
     /// <summary>
-    /// 次のターンに進行する際の処理を実行
+    /// スキルを発動する
     /// </summary>
-    private void MoveNextTurn()
+    private void ExecuteActivateSkillPhase()
     {
-        currentTurnCount++;
-        playerBattleMonsterList.ForEach(m => m.isActed = false);
-        enemyBattleMonsterList.ForEach(m => m.isActed = false);
-        doBattleMonsterIndex = null;
-        beDoneBattleMonsterIndex = null;
+        var doBattleMonster = GetBattleMonster(doBattleMonsterIndex);
+        doBattleMonster.isActed = true;
+
+        // ログを追加
+        AddBattleLog(BattleLogType.StartAttack);
     }
 
     /// <summary>
-    /// 次のウェーブに進行する際の処理を実行
+    /// スキル効果を反映する
     /// </summary>
-    private void MoveNextWave()
+    private void ExecuteReflectSkillPhase()
     {
-        currentWaveCount++;
-        RefreshEnemyBattleMonsterList(currentWaveCount);
-    }
-
-    /// <summary>
-    /// 攻撃処理
-    /// </summary>
-    /// <returns>ダメージ</returns>
-    private int Attack()
-    {
-        if (doBattleMonsterIndex == null || beDoneBattleMonsterIndex == null) return 0;
-
         var doBattleMonster = GetBattleMonster(doBattleMonsterIndex);
         var beDoneBattleMonster = GetBattleMonster(beDoneBattleMonsterIndex);
         var random = new Random();
         var coefficient = 1.0f - (((float)random.NextDouble() * 0.15f) - 0.075f);
-        var damage = doBattleMonster.currentAttack * coefficient;
-        beDoneBattleMonster.currentHp -= (int)damage;
-        doBattleMonster.isActed = true;
-        return (int)damage;
+        var damage = (int)(doBattleMonster.currentAttack * coefficient);
+        beDoneBattleMonster.currentHp -= damage;
+
+        // ログを追加
+        AddBattleLog(BattleLogType.TakeDamage, damage);
     }
 
     /// <summary>
-    /// 指定したログタイプと現在のメンバ変数の値をもとにバトルログ情報を取得
+    /// 勝敗を判定する
     /// </summary>
-    /// <returns></returns>
-    private BattleLogInfo GetCurrentBattleLogInfo(BattleLogType type)
+    private void ExecuteJudgeBattleEndPhase()
+    {
+        if (enemyBattleMonsterList.All(m => m.currentHp <= 0))
+        {
+            // 敵が全滅かつ最後のウェーブであれば勝利
+            if (quest.questWaveIdList.Count == currentWaveCount)
+            {
+                currentWinOrLose = WinOrLose.Win;
+                AddBattleLog(BattleLogType.Result);
+                return;
+            }
+        }
+
+        // 味方が全滅していたら負け
+        if (playerBattleMonsterList.All(m => m.currentHp <= 0))
+        {
+            currentWinOrLose = WinOrLose.Lose;
+            AddBattleLog(BattleLogType.Result);
+            return;
+        }
+
+        // それ以外であれば続行
+        currentWinOrLose = WinOrLose.Continue;
+    }
+
+    private void AddBattleLog(BattleLogType type, int damage = 0)
     {
         var log = "";
-        var damage = 0;
 
-        switch (type) {
+        switch (type)
+        {
             case BattleLogType.StartAttack:
                 log = $"{{do}}の「こうげき」";
                 break;
             case BattleLogType.TakeDamage:
-                damage = Attack();
                 log = $"{{beDone}}に{damage}のダメージ";
                 break;
             case BattleLogType.Die:
@@ -181,7 +237,7 @@ public class BattleDataProcessor
                 break;
         }
 
-        return new BattleLogInfo()
+        var battleLog = new BattleLogInfo()
         {
             type = type,
             playerBattleMonsterList = playerBattleMonsterList.Clone(),
@@ -194,33 +250,7 @@ public class BattleDataProcessor
             log = log,
             damage = damage,
         };
-    }
-
-    /// <summary>
-    /// 次行動するモンスターのバトルモンスターインデックスを取得する
-    /// </summary>
-    private BattleMonsterIndex GetNextActBattleMonsterIndexOrDefault()
-    {
-        var battleMonsterList = new List<BattleMonsterInfo>(playerBattleMonsterList);
-        battleMonsterList.AddRange(enemyBattleMonsterList);
-
-        var battleMonster = battleMonsterList.OrderByDescending(m => m.currentSpeed).Where(m => !m.isActed && m.currentHp > 0).FirstOrDefault();
-        return battleMonster?.index;
-    }
-
-    /// <summary>
-    /// 勝ち負けを取得する
-    /// </summary>
-    private WinOrLose GetWinOrLose()
-    {
-        if(enemyBattleMonsterList.All(m => m.currentHp <= 0))
-        {
-            if (quest.questWaveIdList.Count == currentWaveCount) return WinOrLose.Win;
-        }
-
-        if (playerBattleMonsterList.All(m => m.currentHp <= 0)) return WinOrLose.Lose;
-
-        return WinOrLose.Continue;
+        battleLogList.Add(battleLog);
     }
 
     private void SetPlayerBattleMonsterList(UserMonsterPartyInfo userMonsterParty)
@@ -243,7 +273,7 @@ public class BattleDataProcessor
         var waveIndex = waveCount - 1;
         var questWaveId = quest.questWaveIdList[waveIndex];
         var questWave = MasterRecord.GetMasterOf<QuestWaveMB>().Get(questWaveId);
-        
+
         enemyBattleMonsterList.Clear();
         questWave.questMonsterIdList.ForEach((questMonsterId, index) =>
         {
@@ -251,11 +281,73 @@ public class BattleDataProcessor
             if (questMonster != null)
             {
                 var monster = MasterRecord.GetMasterOf<MonsterMB>().Get(questMonster.monsterId);
-                var battleMonster = BattleUtil.GetBattleMonster(monster,questMonster.level, false, index);
+                var battleMonster = BattleUtil.GetBattleMonster(monster, questMonster.level, false, index);
                 enemyBattleMonsterList.Add(battleMonster);
             }
         });
     }
 
-    
+    private BattleMonsterInfo GetBattleMonster(BattleMonsterIndex battleMonsterIndex)
+    {
+        var battleMonsterList = battleMonsterIndex.isPlayer ? playerBattleMonsterList : enemyBattleMonsterList;
+        return battleMonsterList[battleMonsterIndex.index];
+    }
+
+    private BattlePhase GetNextBattlePhase(BattlePhase currentBattlePhase)
+    {
+        var enumCount = Enum.GetNames(typeof(BattlePhase)).Length;
+        var currentNum = (int)currentBattlePhase;
+        var nextNum = currentNum == enumCount - 1 ? 0 : currentNum + 1;
+        return (BattlePhase)nextNum;
+    }
+
+    private enum BattlePhase
+    {
+        /// <summary>
+        /// 行動するモンスターを決める
+        /// </summary>
+        SetDoMonster = 0,
+
+        /// <summary>
+        /// 必要であれば次のウェーブに進む
+        /// </summary>
+        MoveNextWave,
+
+        /// <summary>
+        /// 必要であれば次のターンに進む
+        /// </summary>
+        MoveNextTurn,
+
+        /// <summary>
+        /// 行動するモンスターが使用するスキルを決める
+        /// </summary>
+        SetDoSkill,
+
+        /// <summary>
+        /// スキル対象のモンスターを決める
+        /// </summary>
+        SetBeDoneMonster,
+
+        /// <summary>
+        /// 行動を実行する
+        /// </summary>
+        ActivateSkill,
+
+        /// <summary>
+        /// スキルの効果を反映させる
+        /// </summary>
+        ReflectSkill,
+
+        /// <summary>
+        /// ゲームの勝敗判定を行う
+        /// </summary>
+        JudgeBattleEnd,
+    }
+
+    private enum BattleMonsterActType
+    {
+        PassiveSkill,
+        NormalSkill,
+        UltimateSkill,
+    }
 }
