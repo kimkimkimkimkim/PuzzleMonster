3036c4150967b08dec326b04762596228c925b57
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-03-07 00:41:55 +0900

    状態異常効果発動処理の実装

---
+++ Assets/Scripts/Manager/BattleDataProcessorExecuteBattleCondition.cs
@@ -0,0 +1,146 @@
+﻿using PM.Enum.Battle;
+using System.Collections.Generic;
+using System.Linq;
+/// <summary>
+/// 状態異常効果関係の処理を行うクラス
+/// </summary>
+public partial class BattleDataProcessor
+{
+    private void ExecuteBattleConditionIfNeeded(BattleConditionTriggerType triggerType, BattleMonsterIndex actionMonsterIndex = null, List<BattleMonsterIndex> beDoneActionMonsterIndexList = null)
+    {
+        switch (triggerType) {
+            case BattleConditionTriggerType.OnBattleStart:
+                ExecuteOnBattleStartBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnWaveStart:
+                ExecuteOnWaveStartBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnTurnStart:
+                ExecuteOnTurnStartBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnMeActionStart:
+                ExecuteOnMeActionStartBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnMeActionEnd:
+                ExecuteOnMeActionEndBattleConditionIfNeeded(actionMonsterIndex);
+                break;
+            case BattleConditionTriggerType.OnMeNormalSkillEnd:
+                ExecuteOnMeNormalSkillEndBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnMeUltimateSkillEnd:
+                ExecuteOnMeUltimateSkillEndBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnMeTakeDamageEnd:
+                ExecuteOnMeTakeDamageEndBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnMeTakeActionBefore:
+                ExecuteOnMeTakeActionBeforeBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnMeTakeActionAfter:
+                ExecuteOnMeTakeActionAfterBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.EveryTimeEnd:
+                ExecuteEveryTimeEndBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnMeDeadEnd:
+                ExecuteOnMeDeadEndBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnTurnEnd:
+                ExecuteOnTurnEndBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnWaveEnd:
+                ExecuteOnWaveEndBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.OnRemoved:
+                ExecuteOnRemovedBattleConditionIfNeeded();
+                break;
+            case BattleConditionTriggerType.Always:
+            default:
+                break;
+        }
+    }
+
+    private void ExecuteOnBattleStartBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnWaveStartBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnTurnStartBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeActionStartBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeActionEndBattleConditionIfNeeded(BattleMonsterIndex actionMonsterIndex)
+    {
+        if (actionMonsterIndex == null) return;
+
+        var actionBattleMonster = GetBattleMonster(actionMonsterIndex);
+        if (actionBattleMonster.isDead) return;
+
+        var skillEffectList = actionBattleMonster.battleConditionList
+            .Where(c => c.battleCondition.battleConditionTriggerType == BattleConditionTriggerType.OnMeActionEnd)
+            .Select(c => c.skillEffect)
+            .ToList();
+        if (skillEffectList.Any()) StartActionStream(actionMonsterIndex, BattleActionType.BattleCondition, skillEffectList);
+    }
+
+    private void ExecuteOnMeNormalSkillEndBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeUltimateSkillEndBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeTakeDamageEndBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeTakeActionBeforeBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeTakeActionAfterBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteEveryTimeEndBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeDeadEndBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnTurnEndBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnWaveEndBattleConditionIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnRemovedBattleConditionIfNeeded()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Manager/BattleDataProcessorExecutePassive.cs
@@ -0,0 +1,150 @@
+﻿using PM.Enum.Battle;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// パッシブスキル効果関係の処理を行うクラス
+/// </summary>
+public partial class BattleDataProcessor
+{
+    private void ExecutePassiveIfNeeded(SkillTriggerType triggerType, BattleMonsterIndex actionMonsterIndex = null, List<BattleMonsterIndex> beDoneActionMonsterIndexList = null)
+    {
+        switch (triggerType)
+        {
+            case SkillTriggerType.OnBattleStart:
+                ExecuteOnBattleStartPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnWaveStart:
+                ExecuteOnWaveStartPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnTurnStart:
+                ExecuteOnTurnStartPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnMeActionStart:
+                ExecuteOnMeActionStartPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnMeActionEnd:
+                ExecuteOnMeActionEndPassiveIfNeeded(actionMonsterIndex);
+                break;
+            case SkillTriggerType.OnMeNormalSkillEnd:
+                ExecuteOnMeNormalSkillEndPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnMeUltimateSkillEnd:
+                ExecuteOnMeUltimateSkillEndPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnMeTakeDamageEnd:
+                ExecuteOnMeTakeDamageEndPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnMeTakeActionBefore:
+                ExecuteOnMeTakeActionBeforePassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnMeTakeActionAfter:
+                ExecuteOnMeTakeActionAfterPassiveIfNeeded();
+                break;
+            case SkillTriggerType.EveryTimeEnd:
+                ExecuteEveryTimeEndPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnMeDeadEnd:
+                ExecuteOnMeDeadEndPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnTurnEnd:
+                ExecuteOnTurnEndPassiveIfNeeded();
+                break;
+            case SkillTriggerType.OnWaveEnd:
+                ExecuteOnWaveEndPassiveIfNeeded();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ExecuteOnBattleStartPassiveIfNeeded()
+    {
+        GetAllMonsterList()
+            .ForEach(m => {
+                if (chainParticipantMonsterIndexList.Contains(m.index)) return;
+                if (m.isDead) return;
+
+                var monster = MasterRecord.GetMasterOf<MonsterMB>().Get(m.monsterId);
+                var passiveSkill = MasterRecord.GetMasterOf<PassiveSkillMB>().Get(monster.passiveSkillId);
+                var effectList = passiveSkill.effectList.Where(effect => effect.triggerType == SkillTriggerType.OnBattleStart).Select(effect => (SkillEffectMI)effect).ToList();
+                if (!effectList.Any()) return;
+
+                StartActionStream(m.index, BattleActionType.PassiveSkill, effectList);
+                chainParticipantMonsterIndexList.Clear();
+            });
+    }
+
+    private void ExecuteOnWaveStartPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnTurnStartPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeActionStartPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeActionEndPassiveIfNeeded(BattleMonsterIndex actionMonsterIndex)
+    {
+        if (actionMonsterIndex == null || chainParticipantMonsterIndexList.Contains(actionMonsterIndex)) return;
+
+        var actionBattleMonster = GetBattleMonster(actionMonsterIndex);
+        if (actionBattleMonster.isDead) return;
+
+        var actionMonster = MasterRecord.GetMasterOf<MonsterMB>().Get(actionBattleMonster.monsterId);
+        var passiveSkill = MasterRecord.GetMasterOf<PassiveSkillMB>().Get(actionMonster.passiveSkillId);
+        var skillEffectList = passiveSkill.effectList.Where(effect => effect.triggerType == SkillTriggerType.OnMeActionEnd).Select(effect => (SkillEffectMI)effect).ToList();
+        if (skillEffectList.Any()) StartActionStream(actionMonsterIndex, BattleActionType.PassiveSkill, skillEffectList);
+    }
+
+    private void ExecuteOnMeNormalSkillEndPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeUltimateSkillEndPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeTakeDamageEndPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeTakeActionBeforePassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeTakeActionAfterPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteEveryTimeEndPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnMeDeadEndPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnTurnEndPassiveIfNeeded()
+    {
+
+    }
+
+    private void ExecuteOnWaveEndPassiveIfNeeded()
+    {
+
+    }
+}
