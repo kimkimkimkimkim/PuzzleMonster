be1d44effb5e07660bc32bfae4cb40fc4cc06110
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-03-14 17:05:27 +0900

    状態異常効果発動処理の修正

--- Assets/Scripts/Manager/BattleDataProcessorExecuteBattleCondition.cs
+++ Assets/Scripts/Manager/BattleDataProcessorExecuteBattleCondition.cs
@@ -7,141 +7,25 @@ using System.Linq;
 /// </summary>
 public partial class BattleDataProcessor
 {
-    private void ExecuteBattleConditionIfNeeded(BattleConditionTriggerType triggerType, BattleMonsterIndex actionMonsterIndex = null, List<BattleMonsterIndex> beDoneActionMonsterIndexList = null)
+    private void ExecuteBattleConditionIfNeeded(SkillTriggerType  triggerType, BattleMonsterIndex targetBattleMonsterIndex)
     {
-        switch (triggerType) {
-            case BattleConditionTriggerType.OnBattleStart:
-                ExecuteOnBattleStartBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnWaveStart:
-                ExecuteOnWaveStartBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnTurnStart:
-                ExecuteOnTurnStartBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnMeActionStart:
-                ExecuteOnMeActionStartBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnMeActionEnd:
-                ExecuteOnMeActionEndBattleConditionIfNeeded(actionMonsterIndex);
-                break;
-            case BattleConditionTriggerType.OnMeNormalSkillEnd:
-                ExecuteOnMeNormalSkillEndBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnMeUltimateSkillEnd:
-                ExecuteOnMeUltimateSkillEndBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnMeTakeDamageEnd:
-                ExecuteOnMeTakeDamageEndBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnMeTakeActionBefore:
-                ExecuteOnMeTakeActionBeforeBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnMeTakeActionAfter:
-                ExecuteOnMeTakeActionAfterBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.EveryTimeEnd:
-                ExecuteEveryTimeEndBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnMeDeadEnd:
-                ExecuteOnMeDeadEndBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnTurnEnd:
-                ExecuteOnTurnEndBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnWaveEnd:
-                ExecuteOnWaveEndBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.OnRemoved:
-                ExecuteOnRemovedBattleConditionIfNeeded();
-                break;
-            case BattleConditionTriggerType.Always:
-            default:
-                break;
-        }
-    }
-
-    private void ExecuteOnBattleStartBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnWaveStartBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnTurnStartBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnMeActionStartBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnMeActionEndBattleConditionIfNeeded(BattleMonsterIndex actionMonsterIndex)
-    {
-        if (actionMonsterIndex == null) return;
-
-        var actionBattleMonster = GetBattleMonster(actionMonsterIndex);
-        if (actionBattleMonster.isDead) return;
-
-        var skillEffectList = actionBattleMonster.battleConditionList
+        // インデックスがnullまたはすでにチェインに参加していたら発動しない
+        if (targetBattleMonsterIndex == null || battleChainParticipantList.Any(p => p.battleMonsterIndex.IsSame(targetBattleMonsterIndex) && p.battleActionType == BattleActionType.BattleCondition)) return;
+        
+        var targetBattleMonster = GetBattleMonster(targetBattleMonsterIndex);
+        var skillEffectList = targetBattleMonster.battleConditionList
             .Where(c => c.battleCondition.battleConditionTriggerType == BattleConditionTriggerType.OnMeActionEnd)
+            .Where(c => IsValid(targetBattleMonsterIndex, c.battleCondition.activateConditionType))
             .Select(c => c.skillEffect)
             .ToList();
         if (skillEffectList.Any()) StartActionStream(actionMonsterIndex, BattleActionType.BattleCondition, skillEffectList);
     }
 
-    private void ExecuteOnMeNormalSkillEndBattleConditionIfNeeded()
+    private void ExecuteBattleConditionIfNeeded(SkillTriggerType  triggerType, List<BattleMonsterIndex> targetBattleMonsterIndexList)
     {
-
-    }
-
-    private void ExecuteOnMeUltimateSkillEndBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnMeTakeDamageEndBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnMeTakeActionBeforeBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnMeTakeActionAfterBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteEveryTimeEndBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnMeDeadEndBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnTurnEndBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnWaveEndBattleConditionIfNeeded()
-    {
-
-    }
-
-    private void ExecuteOnRemovedBattleConditionIfNeeded()
-    {
-
+        targetBattleMonsterIndexList.ForEach(index =>
+        {
+            ExecuteBattleConditionIfNeeded(triggerType, index);
+        });
     }
 }
