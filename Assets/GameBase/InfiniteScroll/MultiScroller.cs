32ed88d1121488b03e0a3c01e116e92513ea8714
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2022-09-21 17:52:56 +0900

    クエスト詳細ウィンドウのUI調整

---
+++ Assets/GameBase/InfiniteScroll/MultiScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Linq;
+
+namespace GameBase {
+    public class MultiScroller : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+        private List<IDragHandler> parentDragHandlerList;
+        private List<IBeginDragHandler> parentBeginDragHandlerList;
+        private List<IEndDragHandler> parentEndDragHandlerList;
+
+        private void Start() {
+            parentDragHandlerList = GetComponentsInParent<IDragHandler>().Where(p => !(p is MultiScroller)).ToList();
+            parentBeginDragHandlerList = GetComponentsInParent<IBeginDragHandler>().Where(p => !(p is MultiScroller)).ToList();
+            parentEndDragHandlerList = GetComponentsInParent<IEndDragHandler>().Where(p => !(p is MultiScroller)).ToList();
+        }
+
+        public void OnDrag(PointerEventData pointerEventData) {
+            parentDragHandlerList.ForEach(p => p.OnDrag(pointerEventData));
+        }
+
+        public void OnBeginDrag(PointerEventData pointerEventData) {
+            parentBeginDragHandlerList.ForEach(p => p.OnBeginDrag(pointerEventData));
+        }
+
+        public void OnEndDrag(PointerEventData pointerEventData) {
+            parentEndDragHandlerList.ForEach(p => p.OnEndDrag(pointerEventData));
+        }
+    }
+}
