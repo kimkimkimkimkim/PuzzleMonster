ede884d1c4fa1ed588c189d2d48409329c235a44
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-01-07 00:00:57 +0900

    ダメージ計算の修正

--- Assets/Scripts/Manager/BattleDataProcessorDamageCalculation.cs
+++ Assets/Scripts/Manager/BattleDataProcessorDamageCalculation.cs
@@ -23,13 +23,13 @@ public partial class BattleDataProcessor
 					case ValueTargetType.MyMaxHp:
 					case ValueTargetType.TargetCurrentHP:
 					case ValueTargetType.TargetMaxHp:
-						return new BattleActionValueData(){ value = GetActionValueWithoutFactor(doBattleMonster, beDoneBattleMonster, skillEffect) };
+						return new BattleActionValueData(){ value = GetHpRateDamageValue(doBattleMonster, beDoneBattleMonster, skillEffect) };
 					// それ以外のダメージの場合は含めて計算
 					default:
 						return GetActionValueWithFactor(doBattleMonster, beDoneBattleMonster, skillEffect);
 				}
 			case SkillType.Heal:
-				return new BattleActionValueData(){ value = GetActionValueWithoutFactor(doBattleMonster, beDoneBattleMonster, skillEffect) };
+				return new BattleActionValueData(){ value = GetHealValue(doBattleMonster, beDoneBattleMonster, skillEffect) };
 			case SkillType.ConditionAdd:
 			case SkillType.ConditionRemove:
 			case SkillType.Revive:
@@ -43,22 +43,23 @@ public partial class BattleDataProcessor
 	/// </summary>
 	private BattleActionValueData GetActionValueWithFactor(BattleMonsterInfo doBattleMonster, BattleMonsterInfo beDoneBattleMonster, SkillEffectMI skillEffect)
 	{
-		// Incoming Damage × (1 – Reduce Damage %) × [(1 – Armor Mitigation %  + 70% × Holy Damage % + 30% × Luck Damage % ]
+		// Incoming Damage × (1 – Reduce Damage %) × [((1 – Armor Mitigation %) × (1 - Armor Break %))  + 70% × Holy Damage % + 30% × Luck Damage % ]
 		const float HOLY_DAMAGE_MAGNIFICATION = 70.0f;
 		const float LUCK_DAMAGE_MAGNIFICATION = 30.0f;
 		const float BLOCK_DAMAGE_REDUCE_RATE = 33.0f;
 		
 		var coefficient = GetValueCoefficient(skillEffect);
 		var incomingDamage = IncomingDamage(doBattleMonster,beDoneBattleMonster, skillEffect);
-		var isBlocked = ExecuteProbability(beDoneBattleMonster.blockRate());
+		var isBlocked = ExecuteProbability(beDoneBattleMonster.blockRate() - doBattleMonster.attackAccuracy());
 		var damage =
 			(int)(
 				incomingDamage.damage														// 基準ダメージ
 				* (1 - GetRate(beDoneBattleMonster.damageResistRate()))						// ダメージ軽減分ダメージを軽減
 				* (
 					(1 - ArmorMitigation(beDoneBattleMonster))								// 防御力分ダメージを軽減
-					+ HOLY_DAMAGE_MAGNIFICATION * GetRate(doBattleMonster.holyDamageRate()) // 神聖ダメージを上乗せ
-					+ LUCK_DAMAGE_MAGNIFICATION * GetRate(doBattleMonster.luckDamageRate()) // ラックダメージを上乗せ
+					* (1 - GetRate(doBattleMonster.armorBreakRate()))						// 防御貫通率分防御力を無視
+					+ HOLY_DAMAGE_MAGNIFICATION * GetRate(doBattleMonster.holyDamageRate()) // 神聖ダメージを加算
+					+ LUCK_DAMAGE_MAGNIFICATION * GetRate(doBattleMonster.luckDamageRate()) // ラックダメージを加算
 				)
 				* (
 					isBlocked ?																// ブロックしたかを判定
@@ -74,12 +75,27 @@ public partial class BattleDataProcessor
 	}
 
 	/// <summary>
-	/// 様々な要因を加味しないアクション値を取得する
+	/// HP割合攻撃によるダメージを取得する
 	/// </summary>
-	private int GetActionValueWithoutFactor(BattleMonsterInfo doBattleMonster, BattleMonsterInfo beDoneBattleMonster, SkillEffectMI skillEffect)
+	private int GetHpRateDamageValue(BattleMonsterInfo doBattleMonster, BattleMonsterInfo beDoneBattleMonster, SkillEffectMI skillEffect)
 	{
 		var coefficient = GetValueCoefficient(skillEffect);
 		return (int)(coefficient * GetStatusValue(doBattleMonster, beDoneBattleMonster, skillEffect) * GetRate(skillEffect.value));
+	}
+
+	/// <summary>
+	/// 回復値を取得する
+	/// </summary>
+	private int GetHealValue(BattleMonsterInfo doBattleMonster, BattleMonsterInfo beDoneBattleMonster, SkillEffectMI skillEffect)
+	{
+		var coefficient = GetValueCoefficient(skillEffect);
+		return (int)(
+			coefficient 
+			* GetStatusValue(doBattleMonster, beDoneBattleMonster, skillEffect)							  // 対象のステータス値
+			* GetRate(skillEffect.value)																  // ダメージ倍率
+			* (1 + GetRate(doBattleMonster.healingRate()))												  // 与回復率分回復量を上昇
+			* (1 + GetRate(beDoneBattleMonster.healedRate()))											  // 被回復率分回復量を上昇
+		);
 	}
 
 	private (float damage, bool isCritical) IncomingDamage(BattleMonsterInfo doMonster, BattleMonsterInfo beDoneMonster, SkillEffectMI skillEffect)
@@ -96,6 +112,7 @@ public partial class BattleDataProcessor
 			* BuffTypeNumKiller(doMonster, beDoneMonster)												   // 指定バフタイプの個数特攻倍率
 			* MonsterAttributeKiller(doMonster, beDoneMonster)											   // 属性特攻倍率
 			* MonsterAttributeCompatibility(doMonster, beDoneMonster)									   // 属性相性
+			* AttackAccuracyCompatibility(doMonster, beDoneMonster)										   // 攻撃精度倍率
 			* (
 				isCritical ?																			   // クリティカルかどうかを判定
 				(CRITICAL_DAMAGE_MAGNIFICATION + CRITICAL_DAMAGE_COEFFICIANT * doMonster.criticalDamage()) // クリティカルならクリティカルダメージ
@@ -168,59 +185,32 @@ public partial class BattleDataProcessor
 
 	private float MonsterAttributeCompatibility(BattleMonsterInfo doBattleMonster, BattleMonsterInfo beDoneBattleMonster)
 	{
-		const int MONSTER_ATTRIBUTE_COMPATIBILITY_RATE = 40;
+		const int MONSTER_ATTRIBUTE_COMPATIBILITY_RATE = 30;
 		var doMonster = MasterRecord.GetMasterOf<MonsterMB>().Get(doBattleMonster.monsterId);
 		var beDoneMonster = MasterRecord.GetMasterOf<MonsterMB>().Get(beDoneBattleMonster.monsterId);
 
-		var rate = 0;
-		switch (doMonster.attribute) {
-			case MonsterAttribute.Red:
-                if(beDoneMonster.attribute == MonsterAttribute.Green)
-                {
-					rate = MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-                }
-				else if(beDoneMonster.attribute == MonsterAttribute.Blue)
-                {
-					rate = -MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-                }
-				break;
-			case MonsterAttribute.Blue:
-				if (beDoneMonster.attribute == MonsterAttribute.Red)
-				{
-					rate = MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-				}
-				else if (beDoneMonster.attribute == MonsterAttribute.Green)
-				{
-					rate = -MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-				}
-				break;
-			case MonsterAttribute.Green:
-				if (beDoneMonster.attribute == MonsterAttribute.Blue)
-				{
-					rate = MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-				}
-				else if (beDoneMonster.attribute == MonsterAttribute.Red)
-				{
-					rate = -MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-				}
-				break;
-			case MonsterAttribute.Yellow:
-				if (beDoneMonster.attribute == MonsterAttribute.Purple)
-				{
-					rate = MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-				}
-				break;
-			case MonsterAttribute.Purple:
-				if (beDoneMonster.attribute == MonsterAttribute.Yellow)
-				{
-					rate = MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
-				}
-				break;
-		}
+		var rate = doMonster.attribute.IsAdvantageous(beDoneMonster.attribute) ? MONSTER_ATTRIBUTE_COMPATIBILITY_RATE 
+			: doMonster.attribute.IsDisadvantage(beDoneMonster.attribute) ? -MONSTER_ATTRIBUTE_COMPATIBILITY_RATE 
+			: 0;
+
 		return GetRate(rate + 100);
 	}
 
-    private float ArmorMitigation(BattleMonsterInfo beDoneMonster)
+	private float AttackAccuracyCompatibility(BattleMonsterInfo doBattleMonster, BattleMonsterInfo beDoneBattleMonster)
+	{
+		const float MONSTER_ATTRIBUTE_COMPATIBILITY_RATE = 0.3f; // 攻撃精度1%につき何%ダメージアップするか
+		const float ADVANTAGE_PLUS_VALUE = 15.0f; // 属性有利時に攻撃精度が何%上昇するか
+		var doMonster = MasterRecord.GetMasterOf<MonsterMB>().Get(doBattleMonster.monsterId);
+		var beDoneMonster = MasterRecord.GetMasterOf<MonsterMB>().Get(beDoneBattleMonster.monsterId);
+
+		var rate = (float)doBattleMonster.attackAccuracy();
+		if (doMonster.attribute.IsAdvantageous(beDoneMonster.attribute)) rate += ADVANTAGE_PLUS_VALUE;
+		rate *= MONSTER_ATTRIBUTE_COMPATIBILITY_RATE;
+
+		return GetRate(rate + 100);
+	}
+
+	private float ArmorMitigation(BattleMonsterInfo beDoneMonster)
 	{
 		// 防御/(180+22×Level)
 		const float DEFENSE_RESISTIVITY = 180.0f;
@@ -233,6 +223,12 @@ public partial class BattleDataProcessor
 	private float GetRate(int rateStatusValue, bool isClamp = true)
 	{
 		var rate = (float)rateStatusValue / 100;
+		return isClamp ? Mathf.Clamp(rate, 0.0f, 1.0f) : rate;
+	}
+
+	private float GetRate(float rateStatusValue, bool isClamp = true)
+	{
+		var rate = rateStatusValue / 100.0f;
 		return isClamp ? Mathf.Clamp(rate, 0.0f, 1.0f) : rate;
 	}
 
