a2154ed1ca8069a2c6c54291693884b33f3f2248
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-04-10 16:25:31 +0900

    モンスターアイコン表示処理の実装

--- Assets/Scripts/UI/Parts/IconItem.cs
+++ Assets/Scripts/UI/Parts/IconItem.cs
@@ -7,10 +7,12 @@ using PM.Enum.UI;
 using UniRx;
 using System;
 using System.Linq;
+using UnityEngine.U2D;
+using UnityEditor.iOS.Xcode;
+using System.Text.RegularExpressions;
 
 [ResourcePath("UI/Parts/Parts-IconItem")]
-public class IconItem : MonoBehaviour
-{
+public class IconItem : MonoBehaviour {
     [SerializeField] protected Image _backgroundImage;
     [SerializeField] protected Image _frameImage;
     [SerializeField] protected Image _iconImage;
@@ -38,16 +40,20 @@ public class IconItem : MonoBehaviour
 
     private ItemType itemType;
     private long itemId;
+    private List<MonsterSpriteInfo> monsterSpriteInfoList;
     private IDisposable onClickButtonObservable;
     private IDisposable onLongClickButtonObservable;
+    private IDisposable monsterAnimationObservable;
 
-    public void ShowIcon(bool isShow)
-    {
+    public void ShowIcon(bool isShow) {
         _iconImage.gameObject.SetActive(isShow);
     }
 
-    public void SetIcon(ItemMI item, bool showNumTextAtOne = false)
-    {
+    public void SetIcon(ItemMI item, bool showNumTextAtOne = false) {
+        SetIconObservable(item, showNumTextAtOne).Subscribe().AddTo(this);
+    }
+
+    public IObservable<Unit> SetIconObservable(ItemMI item, bool showNumTextAtOne = false) {
         itemType = item.itemType;
         itemId = item.itemId;
 
@@ -56,69 +62,183 @@ public class IconItem : MonoBehaviour
         var numText = item.num <= 1 && !showNumTextAtOne ? "" : item.num.ToString();
 
         SetFrameImage(iconColorType);
-        SetIconImage(iconImageType, item.itemId);
         SetNumText(numText);
         SetShowItemDetailDialogAction(true);
 
-        if(itemType == ItemType.Monster) {
+        if (itemType == ItemType.Monster) {
             var monster = MasterRecord.GetMasterOf<MonsterMB>().Get(itemId);
             var maxGrade = MasterRecord.GetMasterOf<GradeUpTableMB>().GetAll().Max(m => m.targetGrade);
             var maxLevel = MasterRecord.GetMasterOf<MaxMonsterLevelMB>().GetAll().First(m => m.monsterRarity == monster.rarity && m.monsterGrade == maxGrade).maxMonsterLevel;
             var userMonsterCustomData = new UserMonsterCustomData() { level = maxLevel, grade = maxGrade };
             var userMonster = new UserMonsterInfo() { monsterId = itemId, customData = userMonsterCustomData };
             SetMonsterIconInfo(userMonster);
+            return SetMonsterIconImageObservable(itemId);
+        } else {
+            return SetIconImageObservable(iconImageType, item.itemId);
         }
     }
 
-    public void SetIcon(ItemType itemType, long itemId)
-    {
+    public void SetIcon(ItemType itemType, long itemId) {
         var item = new ItemMI() { itemType = itemType, itemId = itemId };
         SetIcon(item);
     }
 
-    public void SetMonsterIconInfo(UserMonsterInfo userMonster) 
-    {
+    public void SetMonsterIconInfo(UserMonsterInfo userMonster) {
         var action = new Action(() => { MonsterDetailDialogFactory.Create(new MonsterDetailDialogRequest() { userMonster = userMonster, canStrength = false }).Subscribe(); });
-        
+
         SetLevelText(userMonster.customData.level);
         SetGrade(userMonster.customData.grade);
         SetLongClickAction(true, action);
     }
 
-    private void SetFrameImage(IconColorType iconColor)
-    {
+    private void SetFrameImage(IconColorType iconColor) {
         var index = (int)iconColor;
         _frameImage.sprite = _frameSpriteList[index];
         _backgroundImage.color = _backgroundColorList[index];
     }
 
-    private void SetIconImage(IconImageType iconImageType, long itemId)
-    {
-        PMAddressableAssetUtil.GetIconImageSpriteObservable(iconImageType, itemId)
-            .Do(sprite =>
-            {
-                if(sprite != null)_iconImage.sprite = sprite;
+    private IObservable<Unit> SetIconImageObservable(IconImageType iconImageType, long itemId) {
+        return PMAddressableAssetUtil.GetIconImageSpriteObservable(iconImageType, itemId)
+            .Do(sprite => {
+                if (sprite != null) _iconImage.sprite = sprite;
             })
+            .AsUnitObservable();
+    }
+
+    private IObservable<Unit> SetMonsterIconImageObservable(long monsterId) {
+        SpriteAtlas spriteAtlas = null;
+        TextAsset textAsset = null;
+        return Observable.WhenAll(
+            PMAddressableAssetUtil.GetMonsterIconSpriteAtlasObservable(monsterId).Do(sa => spriteAtlas = sa).AsUnitObservable(),
+            PMAddressableAssetUtil.GetMonsterSpriteInfoObservable(monsterId).Do(ta => textAsset = ta).AsUnitObservable()
+        )
+            .Do(_ => {
+                if (spriteAtlas != null && textAsset != null) {
+                    monsterSpriteInfoList = new List<MonsterSpriteInfo>();
+                    var textList = textAsset.text.Split('\n').ToList();
+                    var monsterStateNameList = Enum.GetNames(typeof(MonsterState)).Select(name => $"(?<name>{name})_(?<index>...).png").ToList();
+                    var monsterStatePattern = string.Join("|", monsterStateNameList);
+
+                    // スプライト情報からステイトや座標などを取得
+                    textList.ForEach((text, index) => {
+                        var match = Regex.Match(text, monsterStatePattern, RegexOptions.IgnoreCase);
+                        if (match.Success) {
+                            var monsterState = GetMonsterState(match.Groups["name"].ToString());
+                            var success = int.TryParse(match.Groups["index"].ToString().TrimStart('0'), out int stateIndex);
+                            var posAndSizeStr = textList[index + 3];
+                            var posAndSizeMatch = Regex.Match(posAndSizeStr, "{{(?<x>.+),(?<y>.+)},{(?<w>.+),(?<h>.+)}}");
+                            if (posAndSizeMatch.Success) {
+                                var x = int.Parse(posAndSizeMatch.Groups["x"].ToString());
+                                var y = int.Parse(posAndSizeMatch.Groups["y"].ToString());
+                                var w = int.Parse(posAndSizeMatch.Groups["w"].ToString());
+                                var h = int.Parse(posAndSizeMatch.Groups["h"].ToString());
+                                var xIndex = x / (w + 1);
+                                var yIndex = y / (h + 1);
+                                var monsterSpriteInfo = new MonsterSpriteInfo() {
+                                    xIndex = xIndex,
+                                    yIndex = yIndex,
+                                    monsterState = monsterState,
+                                    stateIndex = stateIndex,
+                                };
+                                monsterSpriteInfoList.Add(monsterSpriteInfo);
+                            }
+                        }
+                    });
+
+                    // マルチプルスプライトの分割順に並び替える
+                    monsterSpriteInfoList = monsterSpriteInfoList.OrderBy(i => i.yIndex).ThenBy(i => i.xIndex).ToList();
+
+                    // スプライトとスプライトのインデックスを割り当てる
+                    monsterSpriteInfoList.ForEach((i, index) => {
+                        i.spriteAtlasIndex = index;
+                        i.sprite = spriteAtlas.GetSprite($"{monsterId}_{index}");
+                    });
+                }
+            })
+            .Do(_ => SetMonsterState(MonsterState.Idle));
+    }
+
+    public void SetMonsterState(MonsterState state) {
+        if (monsterSpriteInfoList == null || !monsterSpriteInfoList.Any()) return;
+
+        if (monsterAnimationObservable != null) {
+            Debug.Log("Dispose");
+            monsterAnimationObservable.Dispose();
+        }
+        _iconImage.sprite = monsterSpriteInfoList.First(i => i.monsterState == state).sprite;
+    }
+
+    public void SetMonsterStateAnimation(MonsterState state) {
+        if (monsterSpriteInfoList == null || !monsterSpriteInfoList.Any()) return;
+
+        if (monsterAnimationObservable != null) {
+            Debug.Log("Dispose");
+            monsterAnimationObservable.Dispose();
+        }
+        monsterAnimationObservable = PlayMonsterStateAnimationObservable(state, false)
+            .RepeatSafe()
             .Subscribe()
             .AddTo(this);
     }
 
-    public void SetNumText(string text)
-    {
+    public IObservable<bool> PlayMonsterStateAnimationObservable(MonsterState state, bool isDispose = true) {
+        if (monsterSpriteInfoList == null || !monsterSpriteInfoList.Any()) return Observable.Return(false);
+
+        if (isDispose && monsterAnimationObservable != null) {
+            Debug.Log("Dispose");
+            monsterAnimationObservable.Dispose();
+        }
+        return Observable.Create<bool>(observer => {
+            const float INTERVAL = 0.07f;
+            var spriteList = monsterSpriteInfoList.Where(i => i.monsterState == state).OrderBy(i => i.stateIndex).ToList();
+            monsterAnimationObservable = Observable.Interval(TimeSpan.FromSeconds(INTERVAL))
+                .Do(index => {
+                    _iconImage.sprite = spriteList[(int)index].sprite;
+                })
+                .Take(spriteList.Count)
+                .Buffer(spriteList.Count)
+                .DoOnCompleted(() => {
+                    Debug.Log("Complete");
+                    observer.OnNext(true);
+                    observer.OnCompleted();
+                })
+                .DoOnCancel(() => {
+                    Debug.Log("Cancel");
+                    observer.OnNext(false);
+                })
+                .Subscribe();
+            return Disposable.Empty;
+        });
+    }
+
+    private MonsterState GetMonsterState(string monsterStateName) {
+        foreach (MonsterState state in Enum.GetValues(typeof(MonsterState))) {
+            if (Regex.IsMatch(state.ToString(), monsterStateName, RegexOptions.IgnoreCase)) return state;
+        }
+        return MonsterState.None;
+    }
+
+    private class MonsterSpriteInfo {
+        public int xIndex { get; set; }
+        public int yIndex { get; set; }
+        public MonsterState monsterState { get; set; }
+        public int stateIndex { get; set; }
+        public int spriteAtlasIndex { get; set; }
+        public Sprite sprite { get; set; }
+    }
+
+    public void SetNumText(string text) {
         _numText.text = text;
     }
 
-    public void SetToggleGroup(ToggleGroup toggleGroup)
-    {
+    public void SetToggleGroup(ToggleGroup toggleGroup) {
         _toggle.group = toggleGroup;
     }
 
-    public void SetOnClickAction(Action action)
-    {
+    public void SetOnClickAction(Action action) {
         if (action == null) return;
 
-        if (onClickButtonObservable != null)
-        {
+        if (onClickButtonObservable != null) {
             onClickButtonObservable.Dispose();
             onClickButtonObservable = null;
         }
@@ -128,38 +248,32 @@ public class IconItem : MonoBehaviour
             .Subscribe();
     }
 
-    public void SetRaycastTarget(bool isOn)
-    {
+    public void SetRaycastTarget(bool isOn) {
         _canvasGroup.blocksRaycasts = isOn;
     }
 
-    public void ShowGrayoutPanel(bool isShow, string text = "")
-    {
+    public void ShowGrayoutPanel(bool isShow, string text = "") {
         _grayoutPanel.SetActive(isShow);
         _grayoutText.gameObject.SetActive(!string.IsNullOrWhiteSpace(text));
         _grayoutText.text = text;
     }
 
-    public void ShowLabel(bool isShow, string text = "")
-    {
+    public void ShowLabel(bool isShow, string text = "") {
         _labelPanel.SetActive(isShow);
         _labelText.gameObject.SetActive(!string.IsNullOrWhiteSpace(text));
         _labelText.text = text;
     }
 
-    public void ShowText(bool isShow, string text = "")
-    {
+    public void ShowText(bool isShow, string text = "") {
         _text.gameObject.SetActive(isShow);
         _text.text = text;
     }
 
-    public void ShowCheckImage(bool isShow)
-    {
+    public void ShowCheckImage(bool isShow) {
         _checkImage.gameObject.SetActive(isShow);
     }
 
-    public void SetStack(int stackNum) 
-    {
+    public void SetStack(int stackNum) {
         _stackNumText.text = stackNum.ToString();
     }
 
@@ -167,33 +281,27 @@ public class IconItem : MonoBehaviour
         _stackIconPanel.SetActive(isShow);
     }
 
-    public void SetLevelText(int level) 
-    {
+    public void SetLevelText(int level) {
         _levelText.text = $"Lv.{level}";
     }
 
-    public void ShowLevelText(bool isShow) 
-    {
+    public void ShowLevelText(bool isShow) {
         _levelText.gameObject.SetActive(isShow);
     }
 
-    public void SetGrade(int grade) 
-    {
+    public void SetGrade(int grade) {
         _monsterGradeParts.SetGradeImage(grade);
     }
 
-    public void ShowGrade(bool isShow) 
-    {
+    public void ShowGrade(bool isShow) {
         _monsterGradeParts.gameObject.SetActive(isShow);
     }
 
-    public void SetUnderText(string text) 
-    {
+    public void SetUnderText(string text) {
         _underText.text = text;
     }
 
-    public void ShowUnderText(bool isShow) 
-    {
+    public void ShowUnderText(bool isShow) {
         _underText.gameObject.SetActive(isShow);
     }
 
--- Assets/Scripts/Util/PMAddressableAssetUtil.cs
+++ Assets/Scripts/Util/PMAddressableAssetUtil.cs
@@ -4,39 +4,52 @@ using PM.Enum.Item;
 using PM.Enum.Monster;
 using PM.Enum.UI;
 using UniRx;
+using UnityEditor.iOS.Xcode;
 using UnityEngine;
+using UnityEngine.U2D;
 
-public class PMAddressableAssetUtil
-{
+public class PMAddressableAssetUtil {
     private const string ASSET_PATH_PREFIX = "Assets/Contents/PMResources";
 
     /// <summary>
     /// アイコン画像を取得する
     /// </summary>
     /// <param name="itemId">Master:マスタのID、 Enum:(int)Enumした値</param>
-    public static IObservable<Sprite> GetIconImageSpriteObservable(IconImageType iconImageType, long itemId)
-    {
+    public static IObservable<Sprite> GetIconImageSpriteObservable(IconImageType iconImageType, long itemId) {
         var address = ASSET_PATH_PREFIX + "/IconImage/" + iconImageType.ToString() + "/" + itemId.ToString() + ".png";
         return AddressableAssetController.LoadAssetAsObservable<Sprite>(address);
     }
 
-    public static IObservable<Sprite> GetIconImageSpriteObservable(ItemMI item)
-    {
+    public static IObservable<Sprite> GetIconImageSpriteObservable(ItemMI item) {
         var iconImageType = ClientItemUtil.GetIconImageType(item.itemType);
         return GetIconImageSpriteObservable(iconImageType, item.itemId);
     }
 
-    public static IObservable<Sprite> GetIconImageSpriteObservable(ItemType itemType, long itemId)
-    {
+    public static IObservable<Sprite> GetIconImageSpriteObservable(ItemType itemType, long itemId) {
         var iconImageType = ClientItemUtil.GetIconImageType(itemType);
         return GetIconImageSpriteObservable(iconImageType, itemId);
     }
 
     /// <summary>
+    /// モンスターアイコンのスプライトアトラスを取得する
+    /// </summary>
+    public static IObservable<SpriteAtlas> GetMonsterIconSpriteAtlasObservable(long monsterId) {
+        var address = ASSET_PATH_PREFIX + "/IconImage/MonsterSpriteAtlas/" + monsterId.ToString() + ".spriteatlas";
+        return AddressableAssetController.LoadAssetAsObservable<SpriteAtlas>(address);
+    }
+
+    /// <summary>
+    /// モンスターアイコン情報テキストを取得する
+    /// </summary>
+    public static IObservable<TextAsset> GetMonsterSpriteInfoObservable(long monsterId) {
+        var address = ASSET_PATH_PREFIX + "/MonsterSpriteInfo/" + monsterId.ToString() + ".txt";
+        return AddressableAssetController.LoadAssetAsObservable<TextAsset>(address);
+    }
+
+    /// <summary>
     /// 指定した演出Prefabを取得する
     /// </summary>
-    public static IObservable<T> InstantiateVisualFxItemObservable<T>(Transform parent) where T : MonoBehaviour
-    {
+    public static IObservable<T> InstantiateVisualFxItemObservable<T>(Transform parent) where T : MonoBehaviour {
         // プレハブ名はクラスメイト同じにすること
         var prefabName = typeof(T).Name;
         var address = $"{ASSET_PATH_PREFIX}/VisualFx/{prefabName}.prefab";
@@ -46,8 +59,7 @@ public class PMAddressableAssetUtil
     /// <summary>
     /// 指定した通常攻撃軌跡Prefabを取得する
     /// </summary>
-    public static IObservable<ParticleSystem> InstantiateNormalAttackOrbitObservable(Transform parent, MonsterAttribute attribute)
-    {
+    public static IObservable<ParticleSystem> InstantiateNormalAttackOrbitObservable(Transform parent, MonsterAttribute attribute) {
         var address = $"{ASSET_PATH_PREFIX}/NormalAttackOrbit/{(int)attribute}.prefab";
         return AddressableAssetController.InstantiateAsObservable<ParticleSystem>(address, parent);
     }
@@ -55,8 +67,7 @@ public class PMAddressableAssetUtil
     /// <summary>
     /// 指定した通常攻撃演出Prefabを取得する
     /// </summary>
-    public static IObservable<ParticleSystem> InstantiateNormalAttackFxObservable(Transform parent, long attackId)
-    {
+    public static IObservable<ParticleSystem> InstantiateNormalAttackFxObservable(Transform parent, long attackId) {
         var address = $"{ASSET_PATH_PREFIX}/NormalAttackEffect/Prefab/{attackId}.prefab";
         return AddressableAssetController.InstantiateAsObservable<ParticleSystem>(address, parent);
     }
@@ -64,8 +75,7 @@ public class PMAddressableAssetUtil
     /// <summary>
     /// 通常攻撃演出Prefabを取得する
     /// </summary>
-    public static IObservable<ParticleSystem> InstantiateNormalAttackFxObservable(Transform parent)
-    {
+    public static IObservable<ParticleSystem> InstantiateNormalAttackFxObservable(Transform parent) {
         var address = $"{ASSET_PATH_PREFIX}/NormalAttackEffect/Prefab/1.prefab";
         return AddressableAssetController.InstantiateAsObservable<ParticleSystem>(address, parent);
     }
@@ -74,8 +84,7 @@ public class PMAddressableAssetUtil
     /// 指定したスキル演出Prefabを取得する
     /// Normal,Passive,Ultimate共通
     /// </summary>
-    public static IObservable<ParticleSystem> InstantiateSkillFxObservable(Transform parent, long skillFxId)
-    {
+    public static IObservable<ParticleSystem> InstantiateSkillFxObservable(Transform parent, long skillFxId) {
         var address = $"{ASSET_PATH_PREFIX}/SkillAttackEffect/Prefab/{skillFxId}.prefab";
         return AddressableAssetController.InstantiateAsObservable<ParticleSystem>(address, parent);
     }
@@ -83,20 +92,17 @@ public class PMAddressableAssetUtil
     /// <summary>
     /// スキル演出Prefabを取得する
     /// </summary>
-    public static IObservable<ParticleSystem> InstantiateSkillFxPrefabObservable(Transform parent, long skillFxId)
-    {
+    public static IObservable<ParticleSystem> InstantiateSkillFxPrefabObservable(Transform parent, long skillFxId) {
         var address = $"{ASSET_PATH_PREFIX}/SkillFx/Prefab/{skillFxId}.prefab";
         return AddressableAssetController.InstantiateAsObservable<ParticleSystem>(address, parent);
     }
 
-    public static IObservable<SkillFxItem> InstantiateSkillFxItemObservable(Transform parent, long skillFxId)
-    {
+    public static IObservable<SkillFxItem> InstantiateSkillFxItemObservable(Transform parent, long skillFxId) {
         var address = $"{ASSET_PATH_PREFIX}/SkillFx/Prefab/SkillFxItem.prefab";
         return AddressableAssetController.InstantiateAsObservable<SkillFxItem>(address, parent);
     }
 
-    public static IObservable<Sprite> GetSkillFxSpriteObservable(long skillFxId)
-    {
+    public static IObservable<Sprite> GetSkillFxSpriteObservable(long skillFxId) {
         var address = $"{ASSET_PATH_PREFIX}/SkillFx/Sprite/{skillFxId}.png";
         return AddressableAssetController.LoadAssetAsObservable<Sprite>(address);
     }
