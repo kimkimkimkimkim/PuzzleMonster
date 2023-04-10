6ff34271afb8e9c30b8942f7b592fe2d7ddb655a
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-04-10 09:11:23 +0900

    モンスターアイコンサンプル作成

--- Assets/Scripts/UI/Window/UIScript/HomeWindowUIScript.cs
+++ Assets/Scripts/UI/Window/UIScript/HomeWindowUIScript.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 using System.Linq;
 using PM.Enum.Item;
 using PM.Enum.Date;
+using UnityEngine.U2D;
 
 [ResourcePath("UI/Window/Window-Home")]
 public class HomeWindowUIScript : WindowBase
@@ -19,7 +20,9 @@ public class HomeWindowUIScript : WindowBase
     [SerializeField] protected Transform _monsterAreaParentBase;
     [SerializeField] protected GameObject _missionIconBadge;
     [SerializeField] protected GameObject _presentIconBadge;
-    
+    [SerializeField] protected Image _monsterImage;
+    [SerializeField] protected SpriteAtlas _monsterSpriteAtlas;
+
     private List<Transform> monsterAreaParentList = new List<Transform>();
 
     public override void Init(WindowInfo info)
@@ -47,7 +50,7 @@ public class HomeWindowUIScript : WindowBase
             })
             .Where(res => res.dialogResponseType == DialogResponseType.Yes)
             .SelectMany(_ => MobileAdsManager.Instance.ShowRewardObservable())
-            .SelectMany(_=> ApiConnection.RewardAdGrantReward(_rewardAdButton.rewardAdId))
+            .SelectMany(_ => ApiConnection.RewardAdGrantReward(_rewardAdButton.rewardAdId))
             .SelectMany(res =>
             {
                 var itemList = MasterRecord.GetMasterOf<RewardAdMB>().Get(_rewardAdButton.rewardAdId).itemList.Select(i => (ItemMI)i).ToList();
@@ -69,34 +72,73 @@ public class HomeWindowUIScript : WindowBase
         var resumeUserMonsterPartyId = SaveDataUtil.Battle.GetResumeUserMonsterPartyId();
         var resumeUserBattleId = SaveDataUtil.Battle.GetResumeUserBattleId();
         var resumeBattleLogList = SaveDataUtil.Battle.GetResumeBattleLogList();
-        if (resumeQuestId > 0 && resumeUserMonsterPartyId != string.Empty && resumeUserBattleId != string.Empty && resumeBattleLogList.Any()) {
-            CommonDialogFactory.Create(new CommonDialogRequest() {
+        if (resumeQuestId > 0 && resumeUserMonsterPartyId != string.Empty && resumeUserBattleId != string.Empty && resumeBattleLogList.Any())
+        {
+            CommonDialogFactory.Create(new CommonDialogRequest()
+            {
                 commonDialogType = CommonDialogType.NoAndYes,
                 title = "確認",
                 content = "実行中のバトルがあります\n再開しますか？",
             })
-                .SelectMany(res => {
-                    if (res.dialogResponseType == DialogResponseType.No) {
+                .SelectMany(res =>
+                {
+                    if (res.dialogResponseType == DialogResponseType.No)
+                    {
                         SaveDataUtil.Battle.ClearAllResumeSaveData();
                         return ApiConnection.BattleInterruption(resumeUserBattleId).AsUnitObservable();
-                    } else {
+                    }
+                    else
+                    {
                         return BattleManager.Instance.ResumeBattleObservable(resumeQuestId, resumeUserMonsterPartyId, resumeUserBattleId, resumeBattleLogList);
                     }
                 })
                 .Do(_ => MainSceneManager.Instance.SetIsReadyToShowStackableDialog(true))
                 .Subscribe();
-        } else {
+        }
+        else
+        {
             MainSceneManager.Instance.SetIsReadyToShowStackableDialog(true);
         }
+
+        SetMonster();
+    }
+
+    private void SetMonster()
+    {
+        var spriteList = new List<Sprite>();
+        for (var i = 0; i < _monsterSpriteAtlas.spriteCount; i++)
+        {
+            var spriteName = $"boss_antiswarm_{i}";
+            spriteList.Add(_monsterSpriteAtlas.GetSprite(spriteName));
+        }
+
+        var attackIndexList = new List<int>() { 80, 47, 39, 31, 23, 15, 7, 111, 104, 97, 90, 83, 76, 69, 62, 54, 46, 38, 30, 22, 14, 6, 110, 103, 96, 89, 82, 75, 68, 61, 53, 45, 37, 29, 21, 13, 5, 109, 102, 95, 88, 81, 74 };
+        var idleIndex = 71;
+
+        var frameSeconds = 0.1f;
+        Observable.Interval(TimeSpan.FromSeconds(5.0f))
+            .SelectMany(_ =>
+            {
+                return Observable.Interval(TimeSpan.FromSeconds(frameSeconds))
+                    .Do(index =>
+                    {
+                        Debug.Log(index);
+                        _monsterImage.sprite = spriteList[attackIndexList[(int)index]];
+                    })
+                    .Take(attackIndexList.Count)
+                    .Buffer(attackIndexList.Count);
+            })
+            .Do(_ => _monsterImage.sprite = spriteList[idleIndex])
+            .Subscribe();
     }
 
     private void SetMonsterImage()
     {
         var userMonsterParty = ApplicationContext.userData.userMonsterPartyList?.FirstOrDefault();
-        if(userMonsterParty != null)
+        if (userMonsterParty != null)
         {
             monsterAreaParentList.Clear();
-            foreach(Transform child in _monsterAreaParentBase)
+            foreach (Transform child in _monsterAreaParentBase)
             {
                 monsterAreaParentList.Add(child);
             }
@@ -121,12 +163,11 @@ public class HomeWindowUIScript : WindowBase
         var rewardAd = MasterRecord.GetMasterOf<RewardAdMB>().Get(rewardAdId);
         var grantedNum = DateTimeUtil.GetTermValidUserRewardAdList(rewardAd.termType, ApplicationContext.userData.userRewardAdList).Count;
         var termText = "";
-        switch (rewardAd.termType) 
+        switch (rewardAd.termType)
         {
             case TermType.Day:
                 termText = "本日の残り回数";
                 break;
-
         }
 
         return $"{termText}:{rewardAd.limitNum - grantedNum}/{rewardAd.limitNum}";
