df422025bf665d070b34e5d895fb894a0df1058d
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-04-19 19:25:27 +0900

    ガチャスケジュール対応

--- Assets/Scripts/UI/Window/UIScript/GachaWindowUIScript.cs
+++ Assets/Scripts/UI/Window/UIScript/GachaWindowUIScript.cs
@@ -10,37 +10,47 @@ using UniRx;
 using UnityEngine;
 
 [ResourcePath("UI/Window/Window-Gacha")]
-public class GachaWindowUIScript : WindowBase
-{
+public class GachaWindowUIScript : WindowBase {
     [SerializeField] protected InfiniteScroll _infiniteScroll;
 
     private List<GachaBoxMB> gachaBoxList;
 
-    public override void Init(WindowInfo info)
-    {
+    public override void Init(WindowInfo info) {
         base.Init(info);
 
         RefreshScroll();
     }
 
-    private void RefreshScroll()
-    {
+    private void RefreshScroll() {
         _infiniteScroll.Clear();
 
         gachaBoxList = MasterRecord.GetMasterOf<GachaBoxMB>().GetAll()
-            .Where(gachaBox =>
-            {
-                // ガチャボックスに対応するガチャボックス詳細マスタの中に一つでも表示条件を満たしているものがあれば表示する
-                var gachaBoxDetailList = MasterRecord.GetMasterOf<GachaBoxDetailMB>().GetAll().Where(gachaBoxDetail => gachaBoxDetail.gachaBoxId == gachaBox.id).ToList();
-                return gachaBoxDetailList.Any(gachaBoxDetail => ConditionUtil.IsValid(ApplicationContext.userData, gachaBoxDetail.displayConditionList));
-            })
+            .Where(gachaBox => IsOpened(gachaBox))
             .ToList();
 
         _infiniteScroll.Init(gachaBoxList.Count, OnUpdateItem);
     }
 
-    private void OnUpdateItem(int index, GameObject item)
-    {
+    private bool IsOpened(GachaBoxMB gachaBox) {
+        switch (gachaBox.openType) {
+            case GachaOpenType.Schedule:
+                // 対象のスケジュールが期間内なら表示する
+                var gachaSchedule = MasterRecord.GetMasterOf<GachaScheduleMB>().GetAll().FirstOrDefault(m => m.gachaBoxId == gachaBox.id);
+                if (gachaSchedule != null) {
+                    return DateTimeUtil.GetDateFromMasterString(gachaSchedule.startDate) <= DateTimeUtil.Now && DateTimeUtil.Now < DateTimeUtil.GetDateFromMasterString(gachaSchedule.endDate);
+                } else {
+                    return false;
+                }
+            case GachaOpenType.Condition:
+                // ガチャボックスに対応するガチャボックス詳細マスタの中に一つでも表示条件を満たしているものがあれば表示する
+                var gachaBoxDetailList = MasterRecord.GetMasterOf<GachaBoxDetailMB>().GetAll().Where(gachaBoxDetail => gachaBoxDetail.gachaBoxId == gachaBox.id).ToList();
+                return gachaBoxDetailList.Any(gachaBoxDetail => ConditionUtil.IsValid(ApplicationContext.userData, gachaBoxDetail.displayConditionList));
+            default:
+                return false;
+        }
+    }
+
+    private void OnUpdateItem(int index, GameObject item) {
         if ((gachaBoxList.Count <= index) || (index < 0)) return;
 
         var scrollItem = item.GetComponent<GachaBoxScrollItem>();
@@ -51,10 +61,8 @@ public class GachaWindowUIScript : WindowBase
             .ToList();
 
         scrollItem.SetText(gachaBox.title);
-        scrollItem.SetOnClickEmissionRateButtonAction(new Action(() =>
-        {
-            GachaEmissionRateDialogFactory.Create(new GachaEmissionRateDialogRequest()
-            {
+        scrollItem.SetOnClickEmissionRateButtonAction(new Action(() => {
+            GachaEmissionRateDialogFactory.Create(new GachaEmissionRateDialogRequest() {
                 gachaBoxId = gachaBox.id,
             }).Subscribe();
         }));
@@ -86,50 +94,55 @@ public class GachaWindowUIScript : WindowBase
         var cost = gachaBoxDetail.requiredItem.num;
         var num = gachaBoxDetail.gachaExecuteType.Num();
 
-        CommonDialogFactory.Create(new CommonDialogRequest()
-        {
+        CommonDialogFactory.Create(new CommonDialogRequest() {
             commonDialogType = CommonDialogType.NoAndYes,
             title = "確認",
             content = $"{name}を{cost}個使用してガチャを{num}回まわしますか？",
         })
             .Where(res => res.dialogResponseType == DialogResponseType.Yes)
+            .SelectMany(_ => {
+                var gachaBox = MasterRecord.GetMasterOf<GachaBoxMB>().Get(gachaBoxDetail.gachaBoxId);
+                if (IsOpened(gachaBox)) {
+                    return Observable.Return(true);
+                } else {
+                    var title = "確認";
+                    var content = "ガチャ開催条件を満たしていません";
+                    return CommonDialogFactory.Create(new CommonDialogRequest() { commonDialogType = CommonDialogType.YesOnly, title = title, content = content })
+                        .Do(res => RefreshScroll())
+                        .Select(res => false);
+                }
+            })
+            .Where(isContinued => isContinued)
             .SelectMany(_ => ApiConnection.ExecuteGacha(gachaBoxDetail.id))
-            .SelectMany(res =>
-            {
+            .SelectMany(res => {
                 return FadeManager.Instance.PlayFadeAnimationObservable(1.0f, FADE_ANIMATION_TIME)
-                    .SelectMany(_ =>
-                    {
+                    .SelectMany(_ => {
                         var gachaAnimation = UIManager.Instance.CreateContent<GachaAnimation>(UIManager.Instance.gachaAnimationParent);
                         return gachaAnimation.PlayGachaAnimationObservable();
                     })
                     .Do(_ => RefreshScroll())
                     .Select(_ => res);
             })
-            .SelectMany(res =>
-            {
+            .SelectMany(res => {
                 FadeManager.Instance.PlayFadeAnimationObservable(0.0f, FADE_ANIMATION_TIME).Subscribe();
-                return GachaResultWindowFactory.Create(new GachaResultWindowRequest() { itemList = res.rewardItemList});
+                return GachaResultWindowFactory.Create(new GachaResultWindowRequest() { itemList = res.rewardItemList });
             })
             .Subscribe();
     }
 
-    private IObservable<GrantItemsToUserApiResponse> ExecuteGachaObservable(GachaBoxDetailMB gachaBoxDetail)
-    {
+    private IObservable<GrantItemsToUserApiResponse> ExecuteGachaObservable(GachaBoxDetailMB gachaBoxDetail) {
         var itemId = "";
         var itemIdList = new List<string>() { itemId };
         return ApiConnection.GrantItemsToUser(itemIdList);
     }
 
-    public override void Open(WindowInfo info)
-    {
+    public override void Open(WindowInfo info) {
     }
 
-    public override void Back(WindowInfo info)
-    {
+    public override void Back(WindowInfo info) {
     }
 
-    public override void Close(WindowInfo info)
-    {
+    public override void Close(WindowInfo info) {
         base.Close(info);
     }
 }
