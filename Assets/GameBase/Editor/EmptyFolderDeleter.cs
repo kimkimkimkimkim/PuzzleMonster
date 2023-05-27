cc5597b3283b0b2d119348f738f9a3772a086e24
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2023-05-27 15:00:32 +0900

    空のフォルダ削除

---
+++ Assets/GameBase/Editor/EmptyFolderDeleter.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEditor;
+
+public static class EmptyFolderDeleter
+{
+    [MenuItem("Tools/Delete Empty Folder")]
+    private static void Delete()
+    {
+        DoDelete("Assets");
+
+        AssetDatabase.Refresh();
+    }
+
+    private static void DoDelete(string path)
+    {
+        foreach (var dir in Directory.GetDirectories(path))
+        {
+            DoDelete(dir);
+
+            var files = Directory.GetFiles(dir);
+
+            if (files.Length != 0) continue;
+
+            var dirs = Directory.GetDirectories(dir);
+
+            if (dirs.Length != 0) continue;
+
+            AssetDatabase.DeleteAsset(dir);
+        }
+    }
+}
